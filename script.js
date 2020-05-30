a0e707288a62be4b28386dc7710aa38cfbf007fd
daemon-ic <66153994+daemon-ic@users.noreply.github.com>
2020-05-30 00:52:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function trkNum() {
+    var str = document.getElementById("text1").value;
+    alert("You've entered: "+ str);
+  };
+
+
+function result() {
+   window.location.href='result.html';
+};
