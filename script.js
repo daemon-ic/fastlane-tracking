1c7ce96f394a09112e1905490d9f063dbd59e858
daemon-ic <66153994+daemon-ic@users.noreply.github.com>
2020-06-03 23:44:25 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function trkNum() {
-    var str = document.getElementById("text1").value;
-    alert("You've entered: "+ str);
-  };
+function displayInput () {
+  let pathname = window.location.pathname
+  let trackNum = pathname.split('tracking=')[1];
+
+  document.getElementById("display").innerHTML = trackNum
+
+};
 
 
-function result() {
-   window.location.href='result.html';
+function changeURL () {
+  let trackNum = document.getElementById("input").value
+  let form = this;
+
+  form.action =  window.location.href="result.html" /* TEMPORARY PAGE CHANGE */
+
+
+  
+  /* "https://www.fastlanetracking.com/tracking=" + trackNum; */
+
+
+  form.submit;
 };
